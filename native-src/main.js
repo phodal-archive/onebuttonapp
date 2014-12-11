b06f49e64fcbcb0b407c6dc95f46588fbbab6718
Fengda HUANG <h@phodal.com>
2014-12-11 15:45:51 +0800

    add the zepto & replace the page_action with broser_action

--- native-src/main.js
+++ native-src/main.js
@@ -2,19 +2,25 @@ define([
     'underscore',
     'js/Checker',
     'URIjs/URI',
-    'js/Parser'
-], function(_, Checker, URI, Parser) {
+    'js/Parser',
+    'zepto'
+], function(_, Checker, URI, Parser, $) {
     var DEFAULT_CONFIG = {
         'url_pattern' : '(immobiliare)',
         'local_pattern': 'imm_area'
     };
 
     var check = function (tabId, changeInfo, tab){
-        console.log(Parser.parseWhere(tab.url));
+        //console.log(Parser.parseWhere(tab.url));
+        var result = localStorage.getItem("imm_ultimeRicerche");
+        console.log(result);
+        $("#popup p").append(result);
         if(Checker.checkForValidUrl(tab.url, DEFAULT_CONFIG)){
             chrome.pageAction.show(tabId);
         }
     };
 
     chrome.tabs.onUpdated.addListener(check);
+    chrome.browserAction.setBadgeBackgroundColor({color: '#c8112f'});
+    chrome.browserAction.setBadgeText({text: '99'});
 });
